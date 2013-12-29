affb480b0aa294ba2ff55dde2a78b4ea07c3c787
kevin4fly <kevin4fly@gmail.com>
2013-12-29 20:47:16 +0800

    optimized the bitmap implementation
    1. change the bits of bitmap to allow the last bit to be set/unset
    2. add the implementation for bitmap destroy

--- tools/bitmap.c
+++ tools/bitmap.c
@@ -1,6 +1,6 @@
 #include  "bitmap.h"
 
-void init_bitmap(struct bitmap **bm)
+void bitmap_init(struct bitmap **bm)
 {
     *bm = malloc(sizeof(struct bitmap));
     assert(*bm);
@@ -20,4 +20,9 @@ void clr_bit(struct bitmap *bm, int bit_i)
 int test_bit(const struct bitmap *bm, int bit_i)
 {
     return bm->bits[bit_i>>SHIFT_INT] & (1 << (bit_i & MASK_INT));
+}
+
+void bitmap_destroy(struct bitmap *bm)
+{
+    free(bm);
 }
--- tools/bitmap.h
+++ tools/bitmap.h
@@ -1,18 +1,28 @@
+#ifndef  BITMAP_H
+#define  BITMAP_H
+
 #include  <stdlib.h>
 #include  <assert.h>
 #include  <string.h>
 
-#define   BITSPER_INT     32
+#define   BITS_PER_INT     32
 #define   SHIFT_INT       5
 #define   MASK_INT        0x1f
 #define   BITS            10000000
 
+/**< a simple bitmap implementation
+ * .bits: total bits of a bitmap
+ *
+ * */
 struct bitmap
 {
-    int bits[BITS/BITSPER_INT];
+    int bits[BITS/BITS_PER_INT+1];
 };
 
-void init_bitmap(struct bitmap **bm);
+void bitmap_init(struct bitmap **bm);
 void set_bit(struct bitmap *bm, int bit_i);
 void clr_bit(struct bitmap *bm, int bit_i);
 int test_bit(const struct bitmap *bm, int bit_i);
+void bitmap_destroy(struct bitmap *bm);
+
+#endif  /*BITMAP_H*/
