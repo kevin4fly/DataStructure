24c8b2b9d0cd2e6ba9d8587e9a565303d74b9632
kevin4fly <kevin4fly@gmail.com>
2013-12-29 20:58:02 +0800

    optimized the implementation for hashtable
    1. add the ref_cnt field into hashtable to count the references
    2. the pointer array of hashtable should be applied dynamically
    3. add the hashtable destroy feature

--- tools/hashtable.c
+++ tools/hashtable.c
@@ -5,9 +5,18 @@ int hash(int key)
     return key % MAGICNO;
 }
 
-void htable_init(struct hashtable *ht)
+void htable_init(struct hashtable **ht, void (*value_free)(void *value))
 {
-    memset(ht,0,sizeof(struct hashtable));
+    //alloc space for the hashtable
+    *ht = malloc(sizeof(struct hashtable));
+    assert(*ht);
+    //alloc space for the hashnode pointer array of the hashtable
+    (*ht)->htable = malloc(sizeof(struct hashnode *) * HTSIZE);
+    assert((*ht)->htable);
+    //set all the elements of hashnode pointer array to NULL
+    memset((*ht)->htable,0,(sizeof(struct hashnode *) * HTSIZE));
+    //register the callback function
+    (*ht)->value_free = value_free;
 }
 
 struct hashnode *hashnode_new(int key, void *value)
@@ -15,18 +24,52 @@ struct hashnode *hashnode_new(int key, void *value)
     struct hashnode *new = malloc(sizeof(struct hashnode));
     assert(new);
     new->key = key;
-    char * value_new = strdup((char*)value);
-    assert(value_new);
-    new->value = value_new;
+    new->value = strdup(value);
+    assert(new->value);
+    new->ref_cnt = 1;
     new->next = NULL;
     return new;
+}
+
+void hashnode_free(struct hashtable *ht, struct hashnode *entry)
+{
+    assert(ht);
+    assert(entry);
+    ht->value_free(entry->value);
+    free(entry);
+}
+
+static void hashlist_free(struct hashtable *ht, struct hashnode *list)
+{
+    struct hashnode *cur = list, *cur_next;
+    while( cur != NULL )
+    {
+        cur_next = cur->next;
+        hashnode_free(ht, cur);
+        cur = cur_next;
+    }
+}
+
+void htable_destory(struct hashtable *ht)
+{
+    assert(ht);
+    int i;
+    for( i=0 ; i<HTSIZE ; i++ )
+    {
+        if( ht->htable[i] != NULL )
+        {
+            hashlist_free(ht, ht->htable[i]);
+        }
+    }
+    free(ht->htable);
+    free(ht);
 }
 
 struct hashnode *htable_search(const struct hashtable *ht, int key)
 {
     assert(ht);
     int hash_key = hash(key);
-    struct hashnode *p = ht->htable[hash_key].next;
+    struct hashnode *p = ht->htable[hash_key];
     while( p != NULL )
     {
         if( p->key == key )
@@ -38,44 +81,78 @@ struct hashnode *htable_search(const struct hashtable *ht, int key)
     return NULL;
 }
 
-int htable_add(struct hashtable *ht, struct hashnode *hnode)
+/* if the new key to be added does NOT exist, create a new node and add it
+ * into the hash table, else if it DO exist, the member ref_cnt increases by 1
+ *
+ * */
+void htable_add(struct hashtable *ht,int key, void *value)
 {
     assert(ht);
-    assert(hnode);
-    if( htable_search(ht,hnode->key) == NULL )
+    struct hashnode *entry = htable_search(ht, key);
+    if( entry == NULL )
     {
-        int hash_key = hash(hnode->key);
-        hnode->next = ht->htable[hash_key].next;
-        ht->htable[hash_key].next = hnode;
-        return 1;
+        struct hashnode *new = hashnode_new(key, value);
+        int hash_key = hash(new->key);
+        new->next = ht->htable[hash_key];
+        ht->htable[hash_key] = new;
     }
     else
     {
-        printf("key %d already exist, add failed!\n", hnode->key);
-        return 0;
+        entry->ref_cnt++;
     }
 }
 
+/* if the key to be deleted does NOT exist, return 0 to indicate that delete
+ * failed. otherwise, if the reference count larger than 1, decreasing it by
+ * 1, else remove the hashnode from the list
+ *
+ * */
 int htable_del(struct hashtable *ht, int key, struct hashnode *valptr)
 {
     assert(ht);
     int hash_key = hash(key);
-    struct hashnode *p = &ht->htable[hash_key];
-    while( p->next != NULL )
+    struct hashnode *pre_del = ht->htable[hash_key];
+    if( pre_del == NULL )
     {
-        if( p->next->key == key )
+        valptr = NULL;
+        return -1;
+    }
+    if( pre_del->key == key )
+    {
+        if( valptr != NULL )
         {
-            struct hashnode *del = p->next;
-            p->next = del->next;
+            memcpy(valptr,pre_del,sizeof(struct hashnode));
+            valptr->value = strdup((char *)pre_del->value);
+            assert(valptr->value);
+        }
+        if( --pre_del->ref_cnt == 0 )
+        {
+            ht->htable[hash_key] = pre_del->next;
+            hashnode_free(ht, pre_del);
+        }
+        return 1;
+    }
+    struct hashnode *del = pre_del->next;
+    while( del != NULL )
+    {
+        if( del->key == key )
+        {
             if( valptr != NULL )
             {
-                *valptr = *del;
+                memcpy(valptr,del,sizeof(struct hashnode));
+                valptr->value = strdup((char *)del->value);
+                assert(valptr->value);
             }
-            free(del);
+            if( --del->ref_cnt == 0 )
+            {
+                pre_del->next = del->next;
+                hashnode_free(ht, del);
+            }
             return 1;
         }
-        p = p->next;
+        pre_del = del;
+        del = pre_del->next;
     }
-    printf("key %d does not exist, delete failed!\n", key);
+    valptr = NULL;
     return 0;
 }
--- tools/hashtable.h
+++ tools/hashtable.h
@@ -1,26 +1,54 @@
-#include  <stdlib.h>
-#include  <assert.h>
-#include  <string.h>
-#include  <stdio.h>
+#ifndef   HASHTABLE_h
+#define   HASHTABLE_h
 
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
+#include  <assert.h>
+
+/**< hash table implementation
+ *
+ *   */
+
+//hash table size
 #define   HTSIZE    80
+//the magic number for the hash function
 #define   MAGICNO   79
 
+/* hashnode: the node pointed by the entries in the hash table
+ * .key: to identify the node
+ * .value: the value of the node
+ * .ref_cnt: to count the reference of the node
+ * .next: to point to the next node of the list
+ *
+ * */
 struct hashnode
 {
     int key;
     void *value;
+    int ref_cnt;
     struct hashnode *next;
 };
 
+/* hashtable: the hash table containing the entries pointing to the hash node
+ * .htable: to point to the hashnode pointer table
+ * .value_free: a callback function used to free the space of hashnode's value
+ * member
+ *
+ * */
 struct hashtable
 {
-    struct hashnode htable[HTSIZE];
+    struct hashnode **htable;
+    void (*value_free)(void *value);
 };
 
 int hash(int key);
-void htable_init(struct hashtable *ht);
+void htable_init(struct hashtable **ht, void (*value_free)(void *value));
 struct hashnode *hashnode_new(int key, void *value);
-int htable_add(struct hashtable *ht, struct hashnode *hnode);
+void hashnode_free(struct hashtable *ht, struct hashnode *entry);
+void htable_add(struct hashtable *ht,int key, void *value);
 int htable_del(struct hashtable *ht, int key, struct hashnode *valptr);
 struct hashnode *htable_search(const struct hashtable *ht, int key);
+void htable_destory(struct hashtable *ht);
+
+#endif
